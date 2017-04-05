d271cacebf3ce2894e0108b7ae0c73a0373e02ee
1ambda <1amb4a@gmail.com>
2017-04-06 06:38:53 +0900

    feat: Add sticky footer

--- ghp-viewer/src/app/shared/shared.module.ts
+++ ghp-viewer/src/app/shared/shared.module.ts
@@ -3,14 +3,21 @@ import { RouterModule, } from '@angular/router'
 import { MaterialModule, } from '@angular/material'
 
 import { NavbarComponent, } from './navbar/navbar.component'
+import { FooterComponent, } from './footer/footer.component'
 
 @NgModule({
   imports: [
     RouterModule,
     MaterialModule.forRoot(),
   ],
-  declarations: [ NavbarComponent, ],
-  exports: [ NavbarComponent, ],
+  declarations: [
+    NavbarComponent,
+    FooterComponent,
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent,
+  ],
   entryComponents: [ ],
 })
 export class SharedModule {}
